ed6840b38e851e81897ba6fbce5a57df3329b2d2
moontrade <moonkintrade@gmail.com>
2015-06-09 00:12:22 +0300

    delete unused memory log function

--- main.py
+++ main.py
@@ -136,25 +136,6 @@ def is_gm(text):
         return False
 
 
-def print_mem_usage():
-    """Memory usage of the current process in kilobytes."""
-    status = None
-    result = {'peak': 0, 'rss': 0}
-    try:
-        # This will only work on systems with a /proc file system
-        # (like Linux).
-        status = open('/proc/self/status')
-        for line in status:
-            parts = line.split()
-            key = parts[0][2:-1].lower()
-            if key in result:
-                result[key] = int(parts[1])
-    finally:
-        if status is not None:
-            status.close()
-    print(result)
-
-
 def save_gm_to_file(name, realm, armory_url):
     with open("gms.txt", "a") as f:
         f.write("%s (%s) %s" % (name, realm, armory_url))
